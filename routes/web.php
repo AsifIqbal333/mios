62f63aa7f31fdad52e476dfdfd025c8eb2e70f12
Asif Iqbal <sasifiqbal703@gmail.com>
2024-02-02 19:13:28 +0500

    Admin panel Templating

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\backend\AdminController;
-use App\Http\Controllers\Backend\VendorController;
+
 
 /*
 |--------------------------------------------------------------------------
