b065b43f2bd96804cae536a451bc9509b1f481d6
Asif Iqbal <sasifiqbal703@gmail.com>
2024-02-08 16:47:50 +0500

    Admin login page templating

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,63 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>Login &mdash; Stisla</title>
+@extends('admin.auth.layouts.master')
 
-  <!-- General CSS Files -->
-  <link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css">
-
-  <!-- CSS Libraries -->
-  <link rel="stylesheet" href="assets/modules/bootstrap-social/bootstrap-social.css">
-
-  <!-- Template CSS -->
-  <link rel="stylesheet" href="assets/css/style.css">
-  <link rel="stylesheet" href="assets/css/components.css">
-<!-- Start GA -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-94034622-3');
-</script>
-<!-- /END GA --></head>
-
-<body>
-  <div id="app">
+@section('content')
     <section class="section">
       <div class="container mt-5">
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+              <img src="{{  asset('backend/assets/img/stisla-fill.svg') }}" alt="logo" width="100" class="shadow-light rounded-circle">
             </div>
 
             <div class="card card-primary">
                 <div class="card-header"><h4>Login</h4></div>
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }} class="needs-validation" novalidate="">
+                    <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
                         @csrf
                         <div class="form-group">
                             <label for="email">Email</label>
                             <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" tabindex="1" required autofocus autocomplete="username">
                             @if($errors->has('email'))
-                                <div class="invalid-feedback">
+                                <code>
                                     {{ $errors->first('email') }}
-                                </div>
+                                </code>
                             @endif
                         </div>
 
                         <div class="form-group">
-
-                            @if (Route::has('password.request'))
-                            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                                {{ __('Forgot your password?') }}
-                            </a>
-                        @endif
                             <div class="d-block">
                                 <label for="password" class="control-label">Password</label>
                                 @if (Route::has('password.request'))
@@ -72,9 +39,9 @@
                             <input id="password" type="password" class="form-control" name="password" tabindex="2" required autocomplete="current-password">
                         
                             @if($errors->has('password'))
-                                <div class="invalid-feedback">
+                                <code>
                                     {{ $errors->first('password') }}
-                                </div>
+                                </code>
                             @endif
                         </div>
 
@@ -118,23 +85,4 @@
         </div>
       </div>
     </section>
-  </div>
-
-  <!-- General JS Scripts -->
-  <script src="assets/modules/jquery.min.js"></script>
-  <script src="assets/modules/popper.js"></script>
-  <script src="assets/modules/tooltip.js"></script>
-  <script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
-  <script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-  <script src="assets/modules/moment.min.js"></script>
-  <script src="assets/js/stisla.js"></script>
-  
-  <!-- JS Libraies -->
-
-  <!-- Page Specific JS File -->
-  
-  <!-- Template JS File -->
-  <script src="assets/js/scripts.js"></script>
-  <script src="assets/js/custom.js"></script>
-</body>
-</html>
+@endsection
